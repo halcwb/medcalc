5bfa5fb3373b55b5dede0d1f25e367fb9e6b8fd0
halcwb <halcwb@gmail.com>
2015-06-04 20:44:13 +0200

    Now got jakefile without lint issues

--- Jakefile.js
+++ Jakefile.js
@@ -1,3 +1,6 @@
+/*global desc, task, jake, fail, complete */
+"use strict";
+
 desc("Default build task");
 task("default", ["lint"], function () {
     console.log("\n\nBuild OK");
@@ -6,13 +9,37 @@ task("default", ["lint"], function () {
 desc("Lint everything");
 task("lint", [], function () {
     console.log ("Lint is running");
+
     var lint = require("./build/lint/lint_runner.js");
-    lint.validateFile("Jakefile.js", {}, {});
 
     var list = new jake.FileList();
     list.include("**/*.js");
     list.exclude("node_modules");
-    lint.validateFileList(list, {
+
+    var options = {
+        bitwise: true,
+        curly: false,
+        eqeqeq: true,
+        forin: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        noempty: true,
+        nonew: true,
+        regexp: true,
+        undef: true,
+        strict: true,
+        trailing: true,
         node: true
-    }, {});
+    };
+
+    var globals = {
+        describe: false,
+        it: false,
+        beforeEach: false,
+        afterEach: false
+    };
+
+    lint.validateFileList(list.toArray(), options, globals);
 });

8c10c04e5836da32fbe7b580dcf6843d65cee9d4
halcwb <halcwb@gmail.com>
2015-06-04 20:53:22 +0200

    Refactored jake file

--- Jakefile.js
+++ Jakefile.js
@@ -1,45 +1,54 @@
 /*global desc, task, jake, fail, complete */
 "use strict";
 
-desc("Default build task");
-task("default", ["lint"], function () {
-    console.log("\n\nBuild OK");
-});
+(function () {
+    desc("Default build task");
+    task("default", ["lint"], function () {
+        console.log("\n\nBuild OK");
+    });
 
-desc("Lint everything");
-task("lint", [], function () {
-    console.log ("Lint is running");
+    desc("Lint everything");
+    task("lint", [], function () {
+        console.log("Lint is running");
 
-    var lint = require("./build/lint/lint_runner.js");
+        var lint = require("./build/lint/lint_runner.js");
 
-    var list = new jake.FileList();
-    list.include("**/*.js");
-    list.exclude("node_modules");
+        var list = new jake.FileList();
+        list.include("**/*.js");
+        list.exclude("node_modules");
+        var options = getOptions();
+        var globals = getGlobals();
+        lint.validateFileList(list.toArray(), options, globals);
+    });
 
-    var options = {
-        bitwise: true,
-        curly: false,
-        eqeqeq: true,
-        forin: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        noempty: true,
-        nonew: true,
-        regexp: true,
-        undef: true,
-        strict: true,
-        trailing: true,
-        node: true
-    };
+    function getOptions() {
+        var options = {
+            bitwise: true,
+            curly: false,
+            eqeqeq: true,
+            forin: true,
+            immed: true,
+            latedef: true,
+            newcap: true,
+            noarg: true,
+            noempty: true,
+            nonew: true,
+            regexp: true,
+            undef: true,
+            strict: true,
+            trailing: true,
+            node: true
+        };
+        return options;
+    }
 
-    var globals = {
-        describe: false,
-        it: false,
-        beforeEach: false,
-        afterEach: false
-    };
-
-    lint.validateFileList(list.toArray(), options, globals);
-});
+    function getGlobals() {
+        var globals = {
+            describe: false,
+            it: false,
+            beforeEach: false,
+            afterEach: false
+        };
+        return globals;
+    }
+})();

a409bcb1aec10f1e3833196742a114c423532d65
halcwb <halcwb@gmail.com>
2015-06-04 15:54:40 +0200

    Got lint working

--- Jakefile.js
+++ Jakefile.js
@@ -1,4 +1,11 @@
 desc("Default build task");
-task("default", [], function () {
+task("default", ["lint"], function () {
    console.log("\n\nBuild OK");
+});
+
+desc("Lint everything");
+task("lint", [], function () {
+   console.log ("Lint is running");
+   var lint = require("./build/lint/lint_runner.js");
+   lint.validateFile("Jakefile.js", {}, {});
 });

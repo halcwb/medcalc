66ef978cd2ecffc8ab1c86ad6f4611ac4092deb7
halcwb <halcwb@gmail.com>
2015-06-04 20:35:14 +0200

    Got lint working excluding node_modules

--- Jakefile.js
+++ Jakefile.js
@@ -1,11 +1,18 @@
 desc("Default build task");
 task("default", ["lint"], function () {
-   console.log("\n\nBuild OK");
+    console.log("\n\nBuild OK");
 });
 
 desc("Lint everything");
 task("lint", [], function () {
-   console.log ("Lint is running");
-   var lint = require("./build/lint/lint_runner.js");
-   lint.validateFile("Jakefile.js", {}, {});
+    console.log ("Lint is running");
+    var lint = require("./build/lint/lint_runner.js");
+    lint.validateFile("Jakefile.js", {}, {});
+
+    var list = new jake.FileList();
+    list.include("**/*.js");
+    list.exclude("node_modules");
+    lint.validateFileList(list, {
+        node: true
+    }, {});
 });

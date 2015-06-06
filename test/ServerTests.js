f324fcd936ea5b1c26d02e399a5bad8d130fc3e7
halcwb <halcwb@gmail.com>
2015-06-06 16:03:54 +0200

    Changed name of test test

--- test/ServerTests.js
+++ test/ServerTests.js
@@ -6,7 +6,7 @@
 
 var server = require("../src/server/Server.js");
 
-exports.testNothing = function (test) {
+exports.numberIsThree = function (test) {
     var number = server.getNumber();
     test.equals(number , 3, 'number');
     test.done();

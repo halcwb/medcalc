9d284afcd854e46f25450d107ab49e223e41240c
halcwb <halcwb@gmail.com>
2015-06-06 16:44:51 +0200

    Switched back to test method

--- test/ServerTests.js
+++ test/ServerTests.js
@@ -4,12 +4,10 @@
 
 "use strict";
 
-var assert = require('assert');
 var server = require("../src/server/Server.js");
 
 exports.numberIsThree = function (test) {
     var number = server.getNumber();
-    assert.equal(3, number, 'number');
-//    test.equals(number , 3, 'number');
+    test.equals(number , 3, 'number');
     test.done();
 };

03d2888c6058e4783846d8e0f45c4b503717e4aa
halcwb <halcwb@gmail.com>
2015-06-06 17:08:19 +0200

    Added http spike

---
+++ spikes/node_http/Http_Server.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var http = require('http');
+
+var server = http.createServer();
+
+server.on("request", function (request, response) {
+    console.log("Received request");
+
+    var body = "<html><head><title>Node Http Spike</title></head>" +
+            "<body><p>This is a spike of Node's HTTP server.</p></html></html>";
+
+    response.end(body);
+});
+
+server.listen(3000);
+
+console.log('Server started');
---
+++ spikes/node_http/run.sh
@@ -0,0 +1,1 @@
+node Http_Server.js

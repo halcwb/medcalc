def3018e31232694e98a64fab432085d2710d9b5
halcwb <halcwb@gmail.com>
2015-06-06 20:33:01 +0200

    Second server content test

--- test/ServerTests.js
+++ test/ServerTests.js
@@ -7,20 +7,51 @@
 var http = require('http');
 var server = require("../src/server/Server.js");
 
+exports.setUp = function (begin) {
+    server.start();
+    begin();
+};
+
 exports.tearDown = function (done) {
     server.stop(function () {
         done();
     });
 };
 
-exports.testHttpServerIsCreated = function (test) {
-    server.start();
+exports.test_serverReturnsResponse = function (test) {
     // Make a request
-    http.get("http://localhost:3000", function (response) {
+    var req = http.get("http://localhost:3000");
+    req.on('response', function (response) {
         // Check if there is a response
         test.ok(response, 'Expected response received');
+
         response.on('data', function () {});
         test.done();
+    }).on('error', function (e) {
+        // Fail test when error
+        console.log('Got error:' + e.message);
+        test.fail(e);
+    });
+};
+
+exports.test_serverReturnsHelloWorld = function (test) {
+    // Make a request
+    var req = http.get("http://localhost:3000");
+    req.on('response', function (response) {
+        // Check the status code
+        test.equals(200, response.statusCode);
+
+        response.setEncoding('utf8');
+        response.on('data', function (data) {
+            // Check if the response contains hello world
+            console.log('got data', data);
+//            test.equals('Hello World', data, 'Expected response received');
+        });
+
+        response.on('end', function () {
+            test.done();
+        });
+
     }).on('error', function (e) {
         // Fail test when error
         console.log('Got error:' + e.message);
